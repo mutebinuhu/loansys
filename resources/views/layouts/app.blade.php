dc51020568df4e9fdfddb9d82c23b1fd70d8eb5c
swaggp <mutebinuhu1@gmail.com>
2022-02-08 02:31:43 -0800

    few changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -218,7 +218,7 @@
     var table = $('.data-table').DataTable({
         processing: true,
         serverSide: true,
-        ajax: "{{ route('customers.index') }}",
+        ajax: "/customers",
         columns: [
             {data: 'DT_RowIndex', name: 'DT_RowIndex'},
             {data: 'name', name: 'name'},
