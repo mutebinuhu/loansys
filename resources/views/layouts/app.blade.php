0038fdc2d1d887399f6000d0d33d70cb0bf8c298
swaggp <mutebinuhu1@gmail.com>
2022-02-08 00:06:10 -0800

    few changes

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,13 +12,13 @@
   <!-- Ionicons -->
   <link rel="stylesheet" href="{{URL::asset('icons/ionicons.min.css')}}">
   <!-- Tempusdominus Bootstrap 4 -->
-  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'">
+  <link rel="stylesheet" href="/plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css'">
   <!-- iCheck -->
   <link rel="stylesheet" href="{{URL::asset('plugins/icheck-bootstrap/icheck-bootstrap.min.css')}}">
   <!-- JQVMap -->
   <link rel="stylesheet" href="{{URL::asset('plugins/jqvmap/jqvmap.min.css')}}">
   <!-- Theme style -->
-  <link rel="stylesheet" href="{{URL::asset('dist/css/adminlte.min.css')}}">
+  <link rel="stylesheet" href="/dist/css/adminlte.min.css">
   <!-- overlayScrollbars -->
   <link rel="stylesheet" href="{{URL::asset('plugins/overlayScrollbars/css/OverlayScrollbars.min.css')}}">
   <!-- Daterange picker -->
